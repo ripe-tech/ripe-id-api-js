6e64040471c8daf63914a42cac38b0c18bacddb8
joamag <joamag@gmail.com>
2019-03-25 13:16:34 +0000

    Better API creation

--- js/base.js
+++ js/base.js
@@ -1,6 +1,28 @@
+const RIPEID_BASE_URL = "https://id.platforme.com/api/";
+const RIPEID_LOGIN_URL = "https://id.platforme.com/";
+const RIPEID_SCOPE = ["account.me", "account.acl"];
+
 class API {
-    constructor(baseUrl) {
-        this.baseUrl = baseUrl || "http://www.sapo.pt";
+    constructor(
+        baseUrl = RIPEID_BASE_URL,
+        loginUrl = RIPEID_LOGIN_URL,
+        clientId = null,
+        clientSecret = null,
+        redirectUrl = null,
+        scope = RIPEID_SCOPE,
+        accessToken = null,
+        refreshToken = null,
+        sessionId = null
+    ) {
+        this.baseUrl = baseUrl;
+        this.loginUrl = loginUrl;
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+        this.redirectUrl = redirectUrl;
+        this.scope = scope;
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+        this.sessionId = sessionId;
     }
 }
 
