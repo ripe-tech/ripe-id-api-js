a08d4460f565ba6e969e2825adbef4556e7bcf44
joamag <joamag@hive.pt>
2019-03-26 14:59:27 +0000

    Small code fix

--- examples/simple.js
+++ examples/simple.js
@@ -2,7 +2,6 @@ const ripe = require("..");
 
 async function run() {
     const api = new ripe.API();
-    console.info(api.helloAccount());
     console.info(await api.ping());
     console.info(await api.hello());
     console.info(await api.ip());
