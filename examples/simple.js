15262405674d0ae8b7ace1266752d16490fc9def
joamag <joamag@hive.pt>
2019-03-28 11:15:10 +0000

    New yonius default configs

--- examples/simple.js
+++ examples/simple.js
@@ -1,7 +1,12 @@
+const yonius = require("yonius");
 const ripe = require("..");
 
 async function run() {
-    const api = new ripe.API();
+    const baseUrl = yonius.conf("RIPEID_BASE_URL");
+    const loginUrl = yonius.conf("RIPEID_LOGIN_URL");
+    const clientId = yonius.conf("RIPEID_CLIENT_ID");
+    const clientSecret = yonius.conf("RIPEID_CLIENT_SECRET");
+    const api = new ripe.API(baseUrl, loginUrl, clientId, clientSecret);
     console.info(await api.ip());
     console.info(await api.headers());
     console.info(await api.headers(...[undefined]));
