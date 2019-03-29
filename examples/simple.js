ea201d82c43538649f8fa19e08ef4c378ff5c52d
joamag <joamag@gmail.com>
2019-03-29 08:19:26 +0000

    Small set of changes

--- examples/simple.js
+++ examples/simple.js
@@ -1,6 +1,8 @@
 const ripe = require("..");
+const yonius = require("yonius");
 
 async function run() {
+    await yonius.load();
     const api = new ripe.API();
     console.info(await api.oauthAuthorize());
     /*console.info(await api.ip());
