0865e1a40553f9f78515a4f3f188c4a65f1e3482
joamag <joamag@gmail.com>
2019-04-01 22:35:15 +0100

    Removed unused values

--- examples/simple.js
+++ examples/simple.js
@@ -5,9 +5,6 @@ async function run() {
     await yonius.load();
     const api = new ripe.API();
     console.info(await api.oauthAuthorize());
-    /*console.info(await api.ip());
-    console.info(await api.headers());
-    console.info(await api.headers(...[undefined]));*/
 }
 
 run();
