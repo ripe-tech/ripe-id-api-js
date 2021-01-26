dd551d83fea45f3ddecd063e7563c44787f85d2f
João Magalhães <joamag@gmail.com>
2021-01-26 16:37:25 +0000

    feat: add list and get role

---
+++ js/role.js
@@ -0,0 +1,16 @@
+export const RoleAPI = superclass =>
+    class extends superclass {
+        async listRoles() {
+            const url = this.baseUrl + "roles";
+            const contents = await this.get(url);
+            return contents;
+        }
+
+        async getRole(name) {
+            const url = this.baseUrl + `roles/${name}`;
+            const contents = await this.get(url);
+            return contents;
+        }
+    };
+
+export default RoleAPI;
